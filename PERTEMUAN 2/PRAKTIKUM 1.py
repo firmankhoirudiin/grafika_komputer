26701402bcc6d405d44f821e6380252bc1fa4c3e
firmankhoirudiin <164898122+firmankhoirudiin@users.noreply.github.com>
2025-11-14 11:48:32 +0700

    Add files via upload

---
+++ PERTEMUAN 2/PRAKTIKUM 1.py
@@ -0,0 +1,4 @@
+x = 50
+y =100
+warna = "merah"
+print(f"kordinat titik  ({x}),  {y}) dengan warna {warna}.")
---
+++ PERTEMUAN 2/praktikum 5.py
@@ -0,0 +1,12 @@
+titik1 = [(0 , 0),(50 ,50) , (100, 0)]
+pusat = (0 , 0)
+titik = {"x": 10, "y": 20, "warna": "biru"}
+
+print("menampilkan semua titik: ")
+for i in titik1:
+    print(f"menampilkan titik = {i}")
+    
+
+print("titik pusat: ",pusat)
+
+print(f"Titik ({titik['x']},{titik['y']}) berwarna {titik['warna']}.")
